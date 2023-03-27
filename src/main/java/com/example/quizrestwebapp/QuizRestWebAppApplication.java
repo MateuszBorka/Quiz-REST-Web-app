c22a86c33d2eabb24dfcf7465ee48120440f57a9
MateuszBorka <borko.m.126@gmail.com>
2023-03-27 16:33:39 +0200

    Initial commit

---
+++ src/main/java/com/example/quizrestwebapp/QuizRestWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizrestwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizRestWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizRestWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizrestwebapp/QuizRestWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizrestwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizRestWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
