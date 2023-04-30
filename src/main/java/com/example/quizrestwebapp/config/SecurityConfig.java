1ee3b8da69c567d0ed6266b8ec6d8fd1e7b31eb3
Mateusz <borko.m.126@gmail.com>
2023-05-01 00:10:48 +0300

    Deprecated method @EnableGlobalMethodSecurity is replaced by @EnableWebSecurity

--- src/main/java/com/example/quizrestwebapp/config/SecurityConfig.java
+++ src/main/java/com/example/quizrestwebapp/config/SecurityConfig.java
@@ -4,17 +4,17 @@ import com.example.quizrestwebapp.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableMethodSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
