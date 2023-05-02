5b132e699e4baf5115fb4adb45e2be2e5dbd3320
Mateusz <borko.m.126@gmail.com>
2023-05-03 00:22:00 +0300

    Added postman_collection

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM openjdk:17
-VOLUME /tmp
-EXPOSE 8080
-ARG JAR_FILE=target/Quiz-REST-Web-app-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} app.jar
-CMD ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/quizrestwebapp/domain/RefreshJwtRequest.java
+++ src/main/java/com/example/quizrestwebapp/domain/RefreshJwtRequest.java
@@ -2,11 +2,13 @@ package com.example.quizrestwebapp.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RefreshJwtRequest {
 
     public String refreshToken;
